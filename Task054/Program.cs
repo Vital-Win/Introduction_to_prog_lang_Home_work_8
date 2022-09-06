a85e583432cfe82d087cc271a3856a9b927bc88f
Vitaliy <vinogradovvitaliy2013@gmail.com>
2022-09-06 09:36:57 +0300

    Task054

---
+++ Task054/Program.cs
@@ -0,0 +1,82 @@
+﻿
+// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+
+    int[,] result = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 10);
+        }
+    }
+
+    return result;
+}
+
+
+Console.WriteLine("Введите количество строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+Console.WriteLine("Введите количество столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+
+var array = CreateArrayWithRandomNumbers(m, n);
+Console.WriteLine();
+PrintArray(array);
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = 0;
+                temp = array[i, k];
+                array[i, k] = array[i, k + 1];
+                array[i, k + 1] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+PrintArray(array);
